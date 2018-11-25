0de56b73710b0347f408cc937b1aeeaf1acbb6d1
octap16 <octap17@gmail.com>
2018-11-25 19:19:13 +0200

    resolve problem with filter

--- src/main/java/com/example/twitterClone/controller/MainController.java
+++ src/main/java/com/example/twitterClone/controller/MainController.java
@@ -4,7 +4,9 @@ import com.example.twitterClone.domain.Message;
 import com.example.twitterClone.repository.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
@@ -20,10 +22,20 @@ public class MainController {
         return "greeting";
     }
     @GetMapping("/main")
-    public String main(Map<String, Object> model){
-        Iterable<Message> messages = repository.findAll();
-        model.put("messages",messages);
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
+        Iterable<Message> messages;
+
+        if (filter != null && !filter.isEmpty()) {
+            messages = repository.findByTag(filter);
+        } else {
+            messages = repository.findAll();
+        }
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
+
         return "main";
     }
+
 
 }
